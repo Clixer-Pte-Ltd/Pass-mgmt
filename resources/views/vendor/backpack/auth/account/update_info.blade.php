85c643ebe745db4f89225c09a96c4a440db9894b
Nguyen Trung Thanh <thanhnt@portalbeanz.com>
2018-12-11 17:26:57 +0700

    Update user info

--- resources/views/vendor/backpack/auth/account/update_info.blade.php
+++ resources/views/vendor/backpack/auth/account/update_info.blade.php
@@ -84,6 +84,15 @@
                         <input required class="form-control" type="{{ backpack_authentication_column()=='email'?'email':'text' }}" name="{{ $field }}" value="{{ old($field) ? old($field) : $user->$field }}">
                     </div>
 
+                    <div class="form-group">
+                        @php
+                            $label = 'Phone';
+                            $field = 'phone';
+                        @endphp
+                        <label class="required">{{ $label }}</label>
+                        <input required class="form-control" type="text" name="{{ $field }}" value="{{ old($field) ? old($field) : $user->$field }}">
+                    </div>
+
                     <div class="form-group m-b-0">
                         <button type="submit" class="btn btn-success"><span class="ladda-label"><i class="fa fa-save"></i> {{ trans('backpack::base.save') }}</span></button>
                         <a href="{{ backpack_url() }}" class="btn btn-default"><span class="ladda-label">{{ trans('backpack::base.cancel') }}</span></a>
