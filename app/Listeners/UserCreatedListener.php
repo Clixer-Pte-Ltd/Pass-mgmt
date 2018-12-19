41639cbc0a37a6f0c0b8b21e29a45e7c790e88a1
Nguyen Trung Thanh <thanhnt@portalbeanz.com>
2018-12-19 09:15:46 +0700

    refactor user created listener

--- app/Listeners/UserCreatedListener.php
+++ app/Listeners/UserCreatedListener.php
@@ -25,18 +25,35 @@ class UserCreatedListener
     public function handle($event)
     {
         $user = $event->model;
+        $this->handleTenantAccount($user);
+        $this->handleSubContructorAccount($user);
+        $this->handleAirportTeamAccount($user);
+    }
+
+    private function handleTenantAccount($user)
+    {
         if (isset($user->tenant_id) && !$user->hasRole(TENANT_ROLE)) {
             $user->assignRole(TENANT_ROLE);
             if ($user->is_imported) {
                 event(new AccountImported($user));
+            } else {
             }
         }
+    }
+
+    private function handleSubContructorAccount($user)
+    {
         if (isset($user->sub_constructor_id) && !$user->hasRole(SUB_CONSTRUCTOR_ROLE)) {
             $user->assignRole(SUB_CONSTRUCTOR_ROLE);
             if ($user->is_imported) {
                 event(new AccountImported($user));
+            } else {
             }
         }
+    }
+
+    private function handleAirportTeamAccount($user)
+    {
         if (!isset($user->tenant_id) && !isset($user->sub_constructor_id)) {
             $user->assignRole(AIRPORT_TEAM_ROLE);
         }
