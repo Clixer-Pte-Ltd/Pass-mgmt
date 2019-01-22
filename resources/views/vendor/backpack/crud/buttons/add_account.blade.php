f3fc7e13b81d0c6da762ee33db893664cb5c5392
Nguyen Trung Thanh <thanhnt@portalbeanz.com>
2019-01-22 18:33:14 +0700

    Update button UI

--- resources/views/vendor/backpack/crud/buttons/add_account.blade.php
+++ resources/views/vendor/backpack/crud/buttons/add_account.blade.php
@@ -1,4 +1,4 @@
 @if ($crud->hasAccess('update'))
 	<!-- Single edit button -->
-	<a href="{{ url($crud->route.'/'.$entry->getKey().'/account/create') }}" class="btn btn-xs btn-success"><i class="fa fa-user"></i> Add More Account</a>
+	<a href="{{ url($crud->route.'/'.$entry->getKey().'/account/create') }}" class="btn btn-success grad-success"><i class="fa fa-user"></i> Add More Account</a>
 @endif
