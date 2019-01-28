818fc64b8169ee55fa0206af8ec12e2de659b8d9
Vi Do Minh <dominhvi93@gmail.com>
2019-01-28 16:15:35 +0700

    CAG-59-1 : Style Dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -3,12 +3,14 @@
 @section('header')
     <section class="content-header">
         <h1>
-            {{ trans('backpack::base.dashboard') }}<small>{{ trans('backpack::base.first_page_you_see') }}</small>
+            <span class="text-capitalize">
+                {{ trans('backpack::base.dashboard') }}<small class="content_title_header">{{ trans('backpack::base.first_page_you_see') }}</small>
+            </span>
         </h1>
-        <ol class="breadcrumb">
+       <!--  <ol class="breadcrumb">
             <li><a href="{{ backpack_url() }}">{{ config('backpack.base.project_name') }}</a></li>
             <li class="active">{{ trans('backpack::base.dashboard') }}</li>
-        </ol>
+        </ol> -->
     </section>
 @endsection
 
@@ -29,120 +31,54 @@
     $pass_holders_expireIn4Weeks = $pass_holders->where('pass_expiry_date','<=', Carbon::now()->addWeeks(4))->where('pass_expiry_date','>', Carbon::now());
 @endphp
 @section('content')
+
     <div class="row">
         <div class="col-md-12">
 
             {{--    number  --}}
-            <div class="box" style="color:#ffffff">
-                <div class="row">
-                    {{--Active Pass--}}
-                    @include('dashboard.includes.panel',
-                    ['background_color' => 'rgb(64, 199, 95)',
-                    'icon' => 'fa fa-child',
-                    'number' => $pass_holders->where('status', PASS_STATUS_VALID)->count(),
-                    'title' => 'Active Pass'])
+            <div class="box dashboard dashboardChart">
+                {{--Active Pass--}}
+                @include('dashboard.includes.panel',
+                ['background_color' => 'rgb(64, 199, 95)',
+                'icon' => 'fa fa-child',
+                'number' => $pass_holders->where('status', PASS_STATUS_VALID)->count(),
+                'title' => 'Active Pass'])
 
-                    {{--Pass Expiring Within 4 Weeks--}}
-                    @include('dashboard.includes.panel',
-                    ['background_color' => 'rgb(223, 67, 49)', 'icon' => 'fa fa-user-times',
-                    'number' => $pass_holders_expireIn4Weeks->count(),
-                    'title' => 'Pass Expiring Within 4 Weeks'])
+                {{--Pass Expiring Within 4 Weeks--}}
+                @include('dashboard.includes.panel',
+                ['background_color' => 'rgb(223, 67, 49)', 'icon' => 'fa fa-user-times',
+                'number' => $pass_holders_expireIn4Weeks->count(),
+                'title' => 'Pass Expiring Within 4 Weeks'])
 
-                    {{--Pending Return--}}
-                    @include('dashboard.includes.panel',
-                    ['background_color' => '#337ab7',
-                    'icon' => 'fa fa-mail-reply',
-                    'number' => $pass_holders->where('status', PASS_STATUS_TERMINATED)->count(),
-                    'title' => 'Expired Pending Return'])
-                </div>
+                {{--Pending Return--}}
+                @include('dashboard.includes.panel',
+                ['background_color' => '#337ab7',
+                'icon' => 'fa fa-mail-reply',
+                'number' => $pass_holders->where('status', PASS_STATUS_TERMINATED)->count(),
+                'title' => 'Expired Pending Return'])
             </div>
         </div>
-        <div class="col-md-6">
+        <div class="table_listDashboard">
+            <div class="col-md-6">
 
-            {{--Expiring Pass Within 4 Weeks--}}
-            <div class="box">
-                <div class="box-header text-center">
-                    <h2>Expiring Pass Within 4 Weeks</h2>
-                </div>
-                <div class="box-body">
-                    <div class="table-responsive">
-                        <table class="table no-margin table-striped table-hover">
-                            <thead class="bg-primary">
-                            <tr>
-                                <th>Name</th>
-                                <th>NRIC</th>
-                                <th>Issue Date</th>
-                                <th>Expiry Date</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($pass_holders_expireIn4Weeks as $pass)
-                                <tr>
-                                    <td>{{ $pass->applicant_name }}</td>
-                                    <td>{{ $pass->nric }}</td>
-                                    <td>{{ custom_date_format($pass->created_at) }}</td>
-                                    <td>{{ custom_date_format($pass->pass_expiry_date) }}</td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
+                {{--Expiring Pass Within 4 Weeks--}}
+                <div class="box dashboard">
+                    <div class="box-header text-center">
+                        <h2>Expiring Pass Within 4 Weeks</h2>
                     </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6">
-
-            {{--Sub-Contractor Expiring Pass--}}
-            <div class="box">
-                <div class="box-header text-center">
-                    <h2>Sub-Contractor Expiring Pass</h2>
-                </div>
-                <div class="box-body">
-                    <div class="table-responsive">
-                        <table class="table no-margin table-striped table-hover">
-                            <thead class="bg-primary">
-                            <tr>
-                                <th>Name</th>
-                                <th>Tenant/Sub-Contractor</th>
-                                <th>Contac</th>
-                                <th>Expiry Date</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($pass_holders_expireIn4Weeks as $pass)
+                    <div class="box-body dashboard">
+                        <div class="table-responsive">
+                            <table class="table no-margin table-striped table-hover dashboard">
+                                <thead class="bg-primary">
                                 <tr>
-                                    <td>{{ $pass->applicant_name }}</td>
-                                    <td>{{ isset($pass->company) ? $pass->company->name : '' }}</td>
-                                    <td>{{ custom_date_format($pass->created_at) }}</td>
-                                    <td>{{ custom_date_format($pass->pass_expiry_date) }}</td>
+                                    <th>Name</th>
+                                    <th>NRIC</th>
+                                    <th>Issue Date</th>
+                                    <th>Expiry Date</th>
                                 </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-        <div class="col-md-6">
-            {{--Black listed Pass--}}
-            <div class="box">
-                <div class="box-header text-center">
-                    <h2>Black listed Pass</h2>
-                </div>
-                <div class="box-body">
-                    <div class="table-responsive">
-                        <table class="table no-margin table-striped table-hover">
-                            <thead class="bg-primary">
-                            <tr>
-                                <th>Name</th>
-                                <th>NRIC</th>
-                                <th>Issue Date</th>
-                                <th>Expiry Date</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($pass_holders->where('status', PASS_STATUS_BLACKLISTED) as $pass)
+                                </thead>
+                                <tbody>
+                                @foreach($pass_holders_expireIn4Weeks as $pass)
                                     <tr>
                                         <td>{{ $pass->applicant_name }}</td>
                                         <td>{{ $pass->nric }}</td>
@@ -150,40 +86,107 @@
                                         <td>{{ custom_date_format($pass->pass_expiry_date) }}</td>
                                     </tr>
                                 @endforeach
-                            </tbody>
-                        </table>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-6">
-            {{--Pass Pending Return--}}
-            <div class="box">
-                <div class="box-header text-center">
-                    <h2>Pass Pending Return</h2>
-                </div>
-                <div class="box-body">
-                    <div class="table-responsive">
-                        <table class="table no-margin">
-                            <thead class="bg-primary">
-                            <tr>
-                                <th>Name</th>
-                                <th>NRIC</th>
-                                <th>Issue Date</th>
-                                <th>Expiry Date</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($pass_holders->where('status', PASS_STATUS_TERMINATED) as $pass)
+            <div class="col-md-6">
+
+                {{--Sub-Contractor Expiring Pass--}}
+                <div class="box dashboard">
+                    <div class="box-header text-center">
+                        <h2>Sub-Contractor Expiring Pass</h2>
+                    </div>
+                    <div class="box-body dashboard">
+                        <div class="table-responsive">
+                            <table class="table no-margin table-striped table-hover dashboard">
+                                <thead class="bg-primary">
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Tenant/Sub-Contractor</th>
+                                    <th>Contac</th>
+                                    <th>Expiry Date</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($pass_holders_expireIn4Weeks as $pass)
                                     <tr>
                                         <td>{{ $pass->applicant_name }}</td>
-                                        <td>{{ $pass->nric }}</td>
+                                        <td>{{ isset($pass->company) ? $pass->company->name : '' }}</td>
                                         <td>{{ custom_date_format($pass->created_at) }}</td>
                                         <td>{{ custom_date_format($pass->pass_expiry_date) }}</td>
                                     </tr>
                                 @endforeach
-                            </tbody>
-                        </table>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-md-6">
+                {{--Black listed Pass--}}
+                <div class="box dashboard">
+                    <div class="box-header text-center">
+                        <h2>Black listed Pass</h2>
+                    </div>
+                    <div class="box-body dashboard">
+                        <div class="table-responsive">
+                            <table class="table no-margin table-striped table-hover dashboard">
+                                <thead class="bg-primary">
+                                <tr>
+                                    <th>Name</th>
+                                    <th>NRIC</th>
+                                    <th>Issue Date</th>
+                                    <th>Expiry Date</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($pass_holders->where('status', PASS_STATUS_BLACKLISTED) as $pass)
+                                        <tr>
+                                            <td>{{ $pass->applicant_name }}</td>
+                                            <td>{{ $pass->nric }}</td>
+                                            <td>{{ custom_date_format($pass->created_at) }}</td>
+                                            <td>{{ custom_date_format($pass->pass_expiry_date) }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                {{--Pass Pending Return--}}
+                <div class="box dashboard">
+                    <div class="box-header text-center">
+                        <h2>Pass Pending Return</h2>
+                    </div>
+                    <div class="box-body dashboard">
+                        <div class="table-responsive">
+                            <table class="table no-margin table-striped table-hover dashboard">
+                                <thead class="bg-primary">
+                                <tr>
+                                    <th>Name</th>
+                                    <th>NRIC</th>
+                                    <th>Issue Date</th>
+                                    <th>Expiry Date</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($pass_holders->where('status', PASS_STATUS_TERMINATED) as $pass)
+                                        <tr>
+                                            <td>{{ $pass->applicant_name }}</td>
+                                            <td>{{ $pass->nric }}</td>
+                                            <td>{{ custom_date_format($pass->created_at) }}</td>
+                                            <td>{{ custom_date_format($pass->pass_expiry_date) }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
--- resources/views/vendor/backpack/crud/customize/list.blade.php
+++ resources/views/vendor/backpack/crud/customize/list.blade.php
@@ -4,7 +4,7 @@
 	<section class="content-header">
 	  <h1>
       <span class="text-capitalize">{!! $crud->getHeading() ?? $crud->entity_name_plural !!}</span>
-      <small id="datatable_info_stack">{!! $crud->getSubheading() ?? trans('backpack::crud.all').'<span>'.$crud->entity_name_plural.'</span> '.trans('backpack::crud.in_the_database') !!}.</small>
+      <small id="datatable_info_stack" class="content_title_header">{!! $crud->getSubheading() ?? trans('backpack::crud.all').'<span>'.$crud->entity_name_plural.'</span> '.trans('backpack::crud.in_the_database') !!}.</small>
 	  </h1>
 	  <!-- <ol class="breadcrumb">
 	    <li><a href="{{ url(config('backpack.base.route_prefix'), 'dashboard') }}">{{ trans('backpack::crud.admin') }}</a></li>
