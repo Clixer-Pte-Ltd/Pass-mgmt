be727d2a145cf6d4b2cdf6ddecc7b66e4b176835
manhhd <mynameishoangmanh@gmail.com>
2019-02-14 08:28:09 +0700

    Renew Pass Alert setting frequency send email

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -98,6 +98,7 @@ Route::group([
         Route::get('settings/frequency-email', 'SettingsController@frequencyEmail')->name('admin.setting.frequency-email');
         Route::post('settings/frequency-email/expiring-pass-holder-alert', 'SettingsController@updateExpiringPassHolderAlert')->name('admin.setting.frequency-email.expiring-pass-holder-alert');
         Route::post('settings/frequency-email/terminated-pass-alert', 'SettingsController@updateTerminatedPassAlert')->name('admin.setting.frequency-email.terminated-pass-alert');
+        Route::post('settings/frequency-email/renew-pass-holder-alert', 'SettingsController@updateRenewPassHolderAlert')->name('admin.setting.frequency-email.renew-pass-holder-alert');
     });
 
     Route::group(['middleware' => ['role:' . implodeCag(config('backpack.cag.roles'))]], function () {
