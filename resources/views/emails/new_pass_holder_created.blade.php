2f4cd48008589c5af9e2a49269e3b6c8379f0a36
manhhd <mynameishoangmanh@gmail.com>
2019-03-06 14:16:05 +0700

    edit mail blade

--- resources/views/emails/new_pass_holder_created.blade.php
+++ resources/views/emails/new_pass_holder_created.blade.php
@@ -1,12 +1,23 @@
 <p>Dear {{ $account->name }},</p>
 
-
-<div>New Pass Holder was create.</div>
-<ul>
-    <li>
-        Name: {{ $passHolder->applicant_name }}
-    </li>
-</ul>
+<div>New Pass Holder has been created.</div>
+<br>
+<table style="font-family: arial, sans-serif; border-collapse: collapse; width: 100%; ">
+    <tr style="background: #1975dc; color: #ffffff">
+        <th style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">S/N</th>
+        <th style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">Name</th>
+        <th style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">Pass#</th>
+        <th style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">Expiry Date</th>
+        <th style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">Last Update</th>
+    </tr>
+    <tr style="background: #c3dbf7; color: #000000">
+        <td style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">{{ $passHolder->id }}</td>
+        <td style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">{{ $passHolder->applicant_name }}</td>
+        <td style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">{{ $passHolder->nric }}</td>
+        <td style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">{{ $passHolder->pass_expiry_date }}</td>
+        <td style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">{{ $passHolder->updated_at }}</td>
+    </tr>
+</table>
 
 <p>Thanks.</p>
 
--- resources/views/emails/not_validate_companies.blade.php
+++ resources/views/emails/not_validate_companies.blade.php
@@ -1,13 +1,31 @@
 <p>Dear {{ $account->name }},</p>
 @if ($companies->count() > 0)
     <div>List of company that was not validated</div>
-    <ul>
-    @foreach($companies as $company)
-        <li>
-            Company name: {{ $company->name }}
-        </li>
-    @endforeach
-    </ul>
+    <br>
+    <table style="font-family: arial, sans-serif; border-collapse: collapse; width: 100%; ">
+        <tr style="background: #1975dc; color: #ffffff">
+            <th style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">S/N</th>
+            <th style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">Company Name</th>
+            <th style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">Company Code</th>
+            <th style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">Expiry Date</th>
+            <th style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">Date of Creation</th>
+        </tr>
+        @php
+            $i = 0;
+        @endphp
+        @foreach($companies as $company)
+            <tr style="background: {{ $i%2 == 0 ?'#c3dbf7' : '#ffffff' }}; color: #000000">
+                <td style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">{{ $company->id }}</td>
+                <td style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">{{ $company->name }}</td>
+                <td style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">{{ $company->uen }}</td>
+                <td style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">{{ $company->tenancy_end_date }}</td>
+                <td style=" border: 1px solid #dddddd; text-align: left; padding: 8px;">{{ $company->created_at }}</td>
+            </tr>
+            @php
+                $i++;
+            @endphp
+        @endforeach
+    </table>
 @else
     <div>Every Company have been validated.</div>
 @endif
