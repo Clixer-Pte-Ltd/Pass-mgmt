0b8585d8cf37e8df81a8c06fb70c8492963ad8a5
manhhd <mynameishoangmanh@gmail.com>
2019-05-14 14:18:33 +0700

    fix import validate date pass, tenant, subscontruct

--- app/Imports/TenantsImport.php
+++ app/Imports/TenantsImport.php
@@ -11,6 +11,7 @@ use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithValidation;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\SkipsFailures;
+use Illuminate\Validation\Rule;
 
 class TenantsImport implements ToModel, WithHeadingRow, WithValidation, SkipsOnError, SkipsOnFailure
 {
@@ -25,6 +26,14 @@ class TenantsImport implements ToModel, WithHeadingRow, WithValidation, SkipsOnE
     public function model(array $row)
     {
         try {
+            $tenancyStartDate = Carbon::createFromFormat('d/m/Y', $row['tenancy_start_date']);
+            $tenancyEndDate = Carbon::createFromFormat('d/m/Y', $row['tenancy_end_date']);
+            if ($tenancyEndDate < $tenancyStartDate) {
+                throw new \Exception("Tenant <b>{$row['name']}</b> has tenancy end date must after tenancy start date");
+            };
+            if ($tenancyEndDate < Carbon::now()) {
+                throw new \Exception("Tenant <b>{$row['name']}</b> has tenancy end date must after now");
+            };
             return new Tenant([
                 'name' => $row['name'],
                 'uen' => $row['company_code'],
@@ -43,7 +52,7 @@ class TenantsImport implements ToModel, WithHeadingRow, WithValidation, SkipsOnE
             'name' => 'required',
             'company_code' => 'required|unique:tenants,uen|unique:sub_constructors,uen',
             'tenancy_start_date' => 'required',
-            'tenancy_end_date' => 'required|date|after:today|after:tenancy_start_date',
+            'tenancy_end_date' => 'required',
         ];
     }
 
