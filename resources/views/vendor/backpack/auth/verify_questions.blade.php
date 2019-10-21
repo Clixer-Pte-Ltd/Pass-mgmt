147b502096d56af5880b7f59d707655692430c2c
virals-manhhd <manhhd@viralsoft.vn>
2019-10-21 08:54:29 +0700

    Add slide show

--- resources/views/vendor/backpack/auth/verify_questions.blade.php
+++ resources/views/vendor/backpack/auth/verify_questions.blade.php
@@ -2,11 +2,72 @@
 
 @section('content')
     <div class="row" style="padding-top: 50px">
-        <div class="col-md-6" style="padding: 10px">
-            <div style="width: 100%; text-align: center">
-                <iframe style="margin: 0 auto; width: 90%; height: 460px"
-                        src="https://www.youtube.com/embed/iUbZvwGqrUo?autoplay=1">
-                </iframe>
+        <div class="col-md-6" id="verify_img">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" style="background: #000000" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1" style="background: #000000"></li>
+                    <li data-target="#myCarousel" data-slide-to="2" style="background: #000000"></li>
+                    <li data-target="#myCarousel" data-slide-to="3" style="background: #000000" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="4" style="background: #000000"></li>
+                    <li data-target="#myCarousel" data-slide-to="5" style="background: #000000"></li>
+                    <li data-target="#myCarousel" data-slide-to="6" style="background: #000000" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="7" style="background: #000000"></li>
+                    <li data-target="#myCarousel" data-slide-to="8" style="background: #000000"></li>
+                    <li data-target="#myCarousel" data-slide-to="9" style="background: #000000" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="10" style="background: #000000"></li>
+                    <li data-target="#myCarousel" data-slide-to="11" style="background: #000000"></li>
+                </ol>
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" style="border-radius: 5px">
+                    <div class="item active">
+                        <img src="/images/slide_introduce/Slide1.GIF" alt="Los Angeles" style="width:100%;">
+                    </div>
+                    <div class="item">
+                        <img src="/images/slide_introduce/Slide2.GIF" alt="Los Angeles" style="width:100%;">
+                    </div>
+                    <div class="item">
+                        <img src="/images/slide_introduce/Slide3.GIF" alt="Los Angeles" style="width:100%;">
+                    </div>
+                    <div class="item">
+                        <img src="/images/slide_introduce/Slide4.GIF" alt="Los Angeles" style="width:100%;">
+                    </div>
+                    <div class="item">
+                        <img src="/images/slide_introduce/Slide5.GIF" alt="Los Angeles" style="width:100%;">
+                    </div>
+                    <div class="item">
+                        <img src="/images/slide_introduce/Slide6.GIF" alt="Los Angeles" style="width:100%;">
+                    </div>
+                    <div class="item">
+                        <img src="/images/slide_introduce/Slide7.GIF" alt="Los Angeles" style="width:100%;">
+                    </div>
+                    <div class="item">
+                        <img src="/images/slide_introduce/Slide8.GIF" alt="Los Angeles" style="width:100%;">
+                    </div>
+                    <div class="item">
+                        <img src="/images/slide_introduce/Slide9.GIF" alt="Los Angeles" style="width:100%;">
+                    </div>
+                    <div class="item">
+                        <img src="/images/slide_introduce/Slide10.GIF" alt="Los Angeles" style="width:100%;">
+                    </div>
+                    <div class="item">
+                        <img src="/images/slide_introduce/Slide11.GIF" alt="Los Angeles" style="width:100%;">
+                    </div>
+                    <div class="item">
+                        <img src="/images/slide_introduce/Slide12.GIF" alt="Los Angeles" style="width:100%;">
+                    </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev" style="color: #000000">
+                    <span class="glyphicon glyphicon-chevron-left" style="top: 90%;"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next" style="color: #000000">
+                    <span class="glyphicon glyphicon-chevron-right" style="top: 90%;"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
         </div>
         <div class="col-md-6">
@@ -18,9 +79,9 @@
                     array_pop($questions);
                     array_shift($questions);
                 @endphp
-                <h1 style="text-align: center">Letter of Undertaking</h1>
+                <h1 style="text-align: center; margin: 0; padding-top: 10px">Letter of Undertaking</h1>
                 <h4 style="padding-left: 10px">{{ $first }}</h4>
-                <ol style="padding: 10px;overflow-y: scroll; height:400px;">
+                <ol style="padding: 10px;overflow-y: scroll; height:300px;">
 
                     @foreach($questions as $question)
                         <li class="verify_question_li"><input type="checkbox" class="verify_question" style="display: none"><span>{!! $question !!}</span></li>
@@ -75,6 +136,26 @@
                     e.preventDefault(e);
                 }
             });
+            //Initilize start value to 1 'For Slide1.GIF'
+            let currentIndex = 1;
+            //NOTE: Set this value to the number of slides you have in the presentation.
+            let maxIndex=12;
+            function swapImage(imageIndex){
+                //Check if we are at the last image already, return if we are.
+                if(imageIndex>maxIndex){
+                    currentIndex=maxIndex;
+                    return;
+                }
+                //Check if we are at the first image already, return if we are.
+                if(imageIndex<1){
+                    currentIndex=1;
+                    return;
+                }
+                currentIndex=imageIndex;
+                //Otherwise update mainImage
+                document.getElementById("mainImage").src='Slide' +  currentIndex  + '.GIF';
+                return 0;
+            }
         })
     </script>
 @endsection
@@ -82,9 +163,13 @@
     <style>
         /* css list with numeber circle background -------------- */
         .numberlist{
-            width:600px;
-            background: #fff;
+            width: 90%;
+            background: rgba(255, 255, 255, 0.5);
             border-radius: 5px;
+            height: 500px;
+            -webkit-box-shadow: -1px 3px 16px 5px rgba(0,0,0,0.75);
+            -moz-box-shadow: -1px 3px 16px 5px rgba(0,0,0,0.75);
+            box-shadow: -1px 3px 16px 5px rgba(0,0,0,0.75);
         }
         .numberlist ol{
             counter-reset: li;
@@ -111,7 +196,7 @@
         .numberlist li {
             position: relative;
             padding-left: 20px;
-            background: #ededed;
+            background: rgba(237, 237, 237, 0.6);
             margin: 5px;
             border-radius: 5px;
         }
@@ -125,13 +210,10 @@
             transform: scale(1.5);
         }
         #verify_form {
-            background: #ffffff ;
             color: #000000;
             height: 100px;
+            background: rgba(237, 237, 237, 0.6);
             border-radius: 5px;
-            -webkit-box-shadow: 0px 4px 19px 5px rgba(0,0,0,0.52);
-            -moz-box-shadow: 0px 4px 19px 5px rgba(0,0,0,0.52);
-            box-shadow: 0px 4px 19px 5px rgba(0,0,0,0.52);
         }
         #verify_form div.form-group{
             padding-left: 25px;
@@ -141,6 +223,23 @@
             position: absolute;
             top: 15%;
             left: 0;
+        }
+        #verify_img {
+            position: relative;
+            border-radius: 5px;
+            height: 500px;
+        }
+
+        #verify_img #myCarousel {
+            position: absolute;
+            left: 6%;
+            border-radius: 5px;
+            width: 90%;
+            height: 100%;
+            background: rgba(255, 255, 255, 0.5);
+            -webkit-box-shadow: -1px 3px 16px 5px rgba(0,0,0,0.75);
+            -moz-box-shadow: -1px 3px 16px 5px rgba(0,0,0,0.75);
+            box-shadow: -1px 3px 16px 5px rgba(0,0,0,0.75);
         }
         /* End css list with numeber circle background -------------- */
     </style>
