4ad7b230b1f8d932b6f8689663b7816ce976b7af
virals-manhhd <manhhd@viralsoft.vn>
2019-11-25 15:42:04 +0700

    Fix send mail reset password

---
+++ app/Notifications/ResetPassword.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Notifications;
+
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Auth\Notifications\ResetPassword as ResetPasswordNotification;
+
+class ResetPassword extends ResetPasswordNotification implements ShouldQueue
+{
+    use Queueable;
+}
