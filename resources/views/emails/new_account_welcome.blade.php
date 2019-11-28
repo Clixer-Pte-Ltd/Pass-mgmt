3e33b1465152bf51036adee61c8902556279f0e3
virals-manhhd <manhhd@viralsoft.vn>
2019-11-28 15:17:32 +0700

    Change url welcome

--- resources/views/emails/new_account_welcome.blade.php
+++ resources/views/emails/new_account_welcome.blade.php
@@ -1,6 +1,6 @@
 <p>Dear {{ $account->name }},</p>
 <div>Welcome to Changi Airport Group.</div>
 <div>You have become the admin of {{ $companyName }}</div>
-<div>Please login to {{ route('backpack.dashboard') }}</div>
+<div>Please login to {{ url('/') }}</div>
 
 @include('emails.signature')
